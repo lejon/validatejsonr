1bd84d9d5ca672ac41690e93b22484855385fef5
Leif Jonsson <Leif.Jonsson@gmail.com>
2016-08-09 18:54:21 +0200

    Clearer separation between invalid (wrt schema) and non-well formed (syntactically incorrect) JSON

--- tests/testthat/test-file_io.r
+++ tests/testthat/test-file_io.r
@@ -2,10 +2,11 @@ library(testthat)
 
 context("Validate JSON file IO")
 
-validjson <- system.file("extdata", "item-3.json", package = "JSONValidate")
-invalidjson1 <- system.file("extdata", "item-1.json", package = "JSONValidate")
-invalidjson2 <- system.file("extdata", "item-2.json", package = "JSONValidate")
-schemafile <- system.file("extdata", "schema.json", package = "JSONValidate")
+validjson     <- system.file("extdata", "item-3.json", package = "JSONValidate")
+malformedjson <- system.file("extdata", "malformed-json.json", package = "JSONValidate")
+invalidjson1  <- system.file("extdata", "item-1.json", package = "JSONValidate")
+invalidjson2  <- system.file("extdata", "item-2.json", package = "JSONValidate")
+schemafile    <- system.file("extdata", "schema.json", package = "JSONValidate")
 
 test_that("validate valid", {  
   expect_that(validate_jsonfile_with_schemafile(validjson, schemafile)$value, equals(0))
@@ -13,7 +14,16 @@ test_that("validate valid", {
   expect_that(validate_jsonfile_with_schemafile(validjson, schemafile)$jsonfile, equals(validjson))
 })
 
-test_that("validate broken", {  
-  expect_that(validate_jsonfile_with_schemafile(invalidjson1, schemafile), equals(list(value=1,message="Input is not valid JSON. Error(offset 14): Invalid value.", schema=schemafile, jsonfile=invalidjson1)))
-  expect_that(validate_jsonfile_with_schemafile(invalidjson2, schemafile), equals(list(value=1,message="Invalid schema: #Invalid keyword: required Invalid document: #", schema=schemafile, jsonfile=invalidjson2)))
+test_that("validate malformed", {  
+  expect_that(validate_jsonfile_with_schemafile(invalidjson1, schemafile), equals(list(value=100,message="JSON Input is not well-formed JSON. Error(offset 14): Invalid value.", schema=schemafile, jsonfile=invalidjson1)))
+  expect_that(validate_jsonfile_with_schemafile(malformedjson, schemafile), equals(list(value=100,message="JSON Input is not well-formed JSON. Error(offset 12): Missing a colon after a name of object member.", schema=schemafile, jsonfile=malformedjson)))
+})
+
+test_that("validate invalid", {  
+  expect_that(validate_jsonfile_with_schemafile(invalidjson2, schemafile), equals(list(value=200,message="Invalid schema: #Invalid keyword: required Invalid document: #", schema=schemafile, jsonfile=invalidjson2)))
+})
+
+test_that("validate missing", {  
+  expect_error(validate_jsonfile_with_schemafile("missing", schemafile), "Cannot access JSON file*")
+  expect_error(validate_jsonfile_with_schemafile(validjson, "missing"), "Cannot access JSON Schema*")
 })
--- tests/testthat/test-string_io.r
+++ tests/testthat/test-string_io.r
@@ -2,9 +2,10 @@ library(testthat)
 
 context("Validate JSON string IO")
 
-schemafile <- system.file("extdata", "schema.json", package = "JSONValidate")
+schemafile     <- system.file("extdata", "schema.json", package = "JSONValidate")
 invalidjsonfn1 <- system.file("extdata", "item-1.json", package = "JSONValidate")
 invalidjsonfn2 <- system.file("extdata", "item-2.json", package = "JSONValidate")
+malformedjsonfn  <- system.file("extdata", "malformed-json.json", package = "JSONValidate")
 
 test_that("validate valid", {  
   json_code <- "{\"category\": \"book\", \"price\": 25,  \"title\": \"abrakadabra\"}"
@@ -14,9 +15,14 @@ test_that("validate valid", {
   expect_that(basename(validate_result$jsonfile), equals(json_code))
 })
 
-test_that("validate broken", {  
+test_that("validate malformed", {  
   invalidjson1 <- readChar(invalidjsonfn1, file.info(invalidjsonfn1)$size)
+  malformedjson <- readChar(malformedjsonfn, file.info(malformedjsonfn)$size)
+  expect_that(validate_json_with_schemafile(invalidjson1, schemafile), equals(list(value=100,message="JSON Input is not well-formed JSON. Error(offset 14): Invalid value.", schema=schemafile, jsonfile=invalidjson1)))
+  expect_that(validate_json_with_schemafile(malformedjson, schemafile), equals(list(value=100,message="JSON Input is not well-formed JSON. Error(offset 0): Invalid value.", schema=schemafile, jsonfile=malformedjson)))
+})
+
+test_that("validate invalid", {  
   invalidjson2 <- readChar(invalidjsonfn2, file.info(invalidjsonfn2)$size)
-  expect_that(validate_json_with_schemafile(invalidjson1, schemafile), equals(list(value=1,message="Input is not valid JSON. Error(offset 14): Invalid value.", schema=schemafile, jsonfile=invalidjson1)))
-  expect_that(validate_json_with_schemafile(invalidjson2, schemafile), equals(list(value=1,message="Invalid schema: #Invalid keyword: required Invalid document: #", schema=schemafile, jsonfile=invalidjson2)))
+  expect_that(validate_json_with_schemafile(invalidjson2, schemafile), equals(list(value=200,message="Invalid schema: #Invalid keyword: required Invalid document: #", schema=schemafile, jsonfile=invalidjson2)))
 })
